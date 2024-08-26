4c800564d5c5e2fd52f8f32df695d011994e2c8b
SiZapPaaiGwat <xugaofan@gmail.com>
2024-08-26 16:15:22 +0800

    Remove github links

--- rsync.fish
+++ rsync.fish
@@ -3,10 +3,10 @@
 
 # 定义本地和远程路径
 set LOCAL_PATH "./src/main/resources"
-set REMOTE_PATH "/root/.git/Stirling-PDF/src/main/resources"
-set REMOTE_HOST "root@45.117.97.148"
+set REMOTE_PATH "/root/.git/Stirling-PDF/src/main"
+set REMOTE_HOST "root@165.22.246.78"
 
 # 执行 rsync 命令
-rsync -avz $LOCAL_PATH $REMOTE_HOST:$REMOTE_PATH
+rsync -avzP --delete $LOCAL_PATH $REMOTE_HOST:$REMOTE_PATH
 
 echo "同步完成!"
